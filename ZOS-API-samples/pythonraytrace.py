bug in PRT calculation fix

ZOS-API-samples/pythonraytrace.py
@@ -2,79 +2,86 @@ import zosapi
 from System import Enum, Int32, Double, Array
 import numpy as np
 
-# Some User Imports
-nrays = 11
-parent = 'C:/Users/jaren/Desktop/Polarization-Raytrace/'
-filename = parent+'Hubble_Test.zmx'
-wave = 1
-Px = np.linspace(-1,1,nrays)
-Py = np.linspace(-1,1,nrays)
-Hx = np.zeros(Px.shape)
-Hy = np.zeros(Px.shape)
-# Px,Py = np.meshgrid(Px,Py)
+def ZemaxTraceToSurface(nrays,pth,surf):
+    """Traces to a single surface in Zemax OpticStudio Standalone API.
 
-# add the raytrace.dll from the current directory
-import clr, os
-dll = os.path.join(os.path.dirname(os.path.realpath(__file__)), r'RayTrace.dll')
-# dll = 'RayTrace.dll'
-# dll = r'C:\Users\Michael\Documents\Temp\KA-01651-Matlab-DLL\12114-Matlab DLL\RayTrace.dll';
+    Parameters
+    ----------
+    nrays : int
+        how many rays across the pupil that are traced
+    pth : str
+        full file path from C: to your lens file
+    surf : int
+        surface to trace to in ZOS lens file
 
-clr.AddReference(dll);
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    # Some User Imports
+    nrays = 11
+    parent = 'C:/Users/jaren/Desktop/Polarization-Raytrace/'
+    filename = parent+'Hubble_Test.zmx'
+    wave = 1
+    Px = np.linspace(-1,1,nrays)
+    Py = np.linspace(-1,1,nrays)
+    Hx = np.zeros(Px.shape)
+    Hy = np.zeros(Px.shape)
+    # Px,Py = np.meshgrid(Px,Py)
 
-# import the raytrace namespace
-import BatchRayTrace;
+    # add the raytrace.dll from the current directory
+    import clr, os
+    dll = os.path.join(os.path.dirname(os.path.realpath(__file__)), r'RayTrace.dll')
+    # dll = 'RayTrace.dll'
+    # dll = r'C:\Users\Michael\Documents\Temp\KA-01651-Matlab-DLL\12114-Matlab DLL\RayTrace.dll';
 
-# load the double gauss
-zos = zosapi.App()
-TheSystem = zos.TheSystem
-ZOSAPI = zos.ZOSAPI
-TheSystem.LoadFile(filename, False)
+    clr.AddReference(dll)
 
-# check to make sure the file was loaded
-if TheSystem.LDE.NumberOfSurfaces < 4:
-    print('did not load the file properly')
-    exit()
+    # import the raytrace namespace
+    import BatchRayTrace
 
-# run a sequential ray trace
-maxrays = nrays;        # let's trace the 4 marginal rays
-tool = TheSystem.Tools.OpenBatchRayTrace();
-normUnpol = tool.CreateNormUnpol(maxrays, ZOSAPI.Tools.RayTrace.RaysType.Real, TheSystem.LDE.NumberOfSurfaces - 1)
+    # load the double gauss
+    zos = zosapi.App()
+    TheSystem = zos.TheSystem
+    ZOSAPI = zos.ZOSAPI
+    TheSystem.LoadFile(filename, False)
 
-# pass raytracer to dll
-reader = BatchRayTrace.ReadNormUnpolData(tool, normUnpol)
-reader.ClearData();
+    # check to make sure the file was loaded
+    if TheSystem.LDE.NumberOfSurfaces < 4:
+        print('did not load the file properly')
+        exit()
 
-# initialize the output (the number of indices in the output array is maxseg*maxseg
-maxseg = 4;
-rays = reader.InitializeOutput(maxseg);
+    # run a sequential ray trace
+    maxrays = nrays;        # let's trace the 4 marginal rays
+    tool = TheSystem.Tools.OpenBatchRayTrace()
+    normUnpol = tool.CreateNormUnpol(maxrays, ZOSAPI.Tools.RayTrace.RaysType.Real, TheSystem.LDE.NumberOfSurfaces - 1)
 
-# add the 4 full field marginal rays
-import numpy as np
-reader.AddRay(wave, 
-    Hx, 
-    Hy, 
-    Px, 
-    Py, Enum.Parse(ZOSAPI.Tools.RayTrace.OPDMode, 'None'));
+    # pass raytracer to dll
+    reader = BatchRayTrace.ReadNormUnpolData(tool, normUnpol)
+    reader.ClearData()
 
-# read the segments (array length is maxsegs * max segs)
-isfinished = False
-while not isfinished:
-    segments = reader.ReadNextBlock(rays)
-    if segments == 0:
-        isfinished = True
+    # initialize the output (the number of indices in the output array is maxseg*maxseg
+    maxseg = 4
+    rays = reader.InitializeOutput(maxseg)
 
-# print the rays
-print(list(rays.X))
-print(list(rays.Y))
-print(list(rays.Z))
+    # add the 4 full field marginal rays
+    import numpy as np
+    reader.AddRay(wave, 
+        Hx, 
+        Hy, 
+        Px, 
+        Py, Enum.Parse(ZOSAPI.Tools.RayTrace.OPDMode, 'None'))
 
-print(list(rays.L))
-print(list(rays.M))
-print(list(rays.N))
+    # read the segments (array length is maxsegs * max segs)
+    isfinished = False
+    while not isfinished:
+        segments = reader.ReadNextBlock(rays)
+        if segments == 0:
+            isfinished = True
 
-print(list(rays.l2))
-print(list(rays.m2))
-print(list(rays.n2))
+    # always close your tools
+    tool.Close()
 
-# always close your tools
-tool.Close();
+    # return the rays
+    return rays.X,rays.Y,rays.Z,rays.L,rays.M,rays.N,rays.l2,rays.m2,rays.n2

polutils.py
@@ -72,6 +72,7 @@ def ConvertBatchRayData(fname,n1,n2,mode='transmission'):
     mData = rays[:,4]
     nData = rays[:,5]
 
+
     # Direction cosines of surface normal
     l2Data = rays[:,6]
     m2Data = rays[:,7]
@@ -91,10 +92,10 @@ def ConvertBatchRayData(fname,n1,n2,mode='transmission'):
     aoe_data = np.arccos(numerator/denominator)
     aoe = aoe_data - (aoe_data[0:total_rays_in_both_axes] > np.pi/2) * np.pi
     aoe = np.abs(aoe)
-    aoi = np.arcsin(n2/n1 * np.sin(aoe))
+    aoi = (np.arcsin(n2/n1 * np.sin(aoe)))
 
     # Compute kin with Snell's Law: https://en.wikipedia.org/wiki/Snell%27s_law#Vector_form
-    kout = np.array([lData,mData,n2Data])
+    kout = np.array([lData,mData,nData])
     kout /= np.sqrt(lData**2 + mData**2 + nData**2)
 
     if mode == 'transmission':
@@ -163,12 +164,12 @@ def ConstructOrthogonalTransferMatrices(kin,kout,normal):
     # kout /= np.linalg.norm(kout)
 
     sin = np.cross(kin,normal)
-    # sin /= np.linalg.norm(sin) # normalize the s-vector
+    sin /= np.linalg.norm(sin) # normalize the s-vector
     pin = np.cross(kin,sin)
     Oinv = np.array([sin,pin,kin])
 
-    sout = np.cross(kout,normal)
-    # sout /= np.linalg.norm(sout) # normalize the s-vector
+    sout = sin #np.cross(kout,normal)
+    sout /= np.linalg.norm(sout) # normalize the s-vector
     pout = np.cross(kout,sout)
     Oout = np.transpose(np.array([sout,pout,kout]))
 
@@ -177,7 +178,7 @@ def ConstructOrthogonalTransferMatrices(kin,kout,normal):
 def ConstructPRTMatrix(kin,kout,normal,aoi,n1,n2,mode='reflection'):
 
     # Compute the Fresnel coefficients for either transmission OR reflection
-    fs,fp = FresnelCoefficients(aoi,n1,n2,mode)
+    fs,fp = FresnelCoefficients(aoi,n1,n2,mode=mode)
 
     # Compute the orthogonal transfer matrices
     Oinv,Oout = ConstructOrthogonalTransferMatrices(kin,kout,normal)

pythonraytrace.py
@@ -1,74 +1,186 @@
-from subprocess import NORMAL_PRIORITY_CLASS
-import numpy as np
-import clr, os
-
-# User input (will swap with function later)
-nrays = 11
-parent = 'C:/Users/jaren/Desktop/Polarization-Raytrace/'
-filename = parent+'Hubble_Test.zmx'
-wave = 1
-Hx = np.double(0)
-Hy = np.double(0)
-Px = np.linspace(-1,1,nrays)
-Py = np.linspace(-1,1,nrays)
-Px,Py = np.meshgrid(Px,Py)
-Px = Px.tolist()
-Py = Py.tolist()
-
 import zosapi
 from System import Enum, Int32, Double, Array
+import numpy as np
 
-# add the raytrace.dll from the current directory
-import clr, os
-dll = os.path.join(os.path.dirname(os.path.realpath(__file__)), r'RayTrace.dll')
-# dll = 'RayTrace.dll'
-# dll = r'C:\Users\Michael\Documents\Temp\KA-01651-Matlab-DLL\12114-Matlab DLL\RayTrace.dll';
+def ZOSRayData(nrays,pth,surf,n1,n2,mode='reflection',show_rays=False):
 
-clr.AddReference(dll);
+    xData,yData,zData,lData,mData,nData,l2Data,m2Data,n2Data = ZemaxTraceToSurface(nrays,pth,surf)
 
-# import the raytrace namespace
-import BatchRayTrace;
+    # Sometimes nan values are returned, can we just filter them?
+    # xData = xData[yData != 0]
+    # zData = zData[yData != 0] # using the y data is intentional, apparently the z data 
+    #                           # is less prone to producing zero values
 
-# load the double gauss
-zos = zosapi.App()
-TheSystem = zos.TheSystem
-ZOSAPI = zos.ZOSAPI
-TheSystem.LoadFile(os.path.join(zos.TheApplication.SamplesDir, r'Sequential\Objectives\Double Gauss 28 degree field.zmx'), False)
+    # lData = lData[yData != 0]
+    # nData = nData[yData != 0]
+    # mData = mData[yData != 0]
+    
+    # l2Data = l2Data[yData != 0]
+    # n2Data = n2Data[yData != 0]
+    # m2Data = m2Data[yData != 0]
+    # yData = yData[yData != 0]
 
-# check to make sure the file was loaded
-if TheSystem.LDE.NumberOfSurfaces < 4:
-    print('did not load the file properly')
-    exit()
+    # Which one is the most restrictive?
+    print(xData.shape)
+    print(yData.shape)
+    print(zData.shape)
+    print(lData.shape)
+    print(mData.shape)
+    print(nData.shape)
+    print(l2Data.shape)
+    print(m2Data.shape)
+    print(n2Data.shape)
 
-# run a sequential ray trace
-maxrays = nrays;        # let's trace the 4 marginal rays
-tool = TheSystem.Tools.OpenBatchRayTrace();
-normUnpol = tool.CreateNormUnpol(maxrays, ZOSAPI.Tools.RayTrace.RaysType.Real, TheSystem.LDE.NumberOfSurfaces - 1)
+    if show_rays == True:
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=[15,5])
+        plt.subplot(131)
+        plt.xlabel('X Data')
+        plt.ylabel('y Data')
+        plt.scatter(xData,yData)
+        plt.subplot(132)
+        plt.xlabel('L Data')
+        plt.ylabel('M Data')
+        plt.scatter(lData,mData)
+        plt.subplot(133)
+        plt.xlabel('L2 Data')
+        plt.ylabel('M2 Data')
+        plt.scatter(l2Data,m2Data)
+        plt.show()
 
-# pass raytracer to dll
-reader = BatchRayTrace.ReadNormUnpolData(tool, normUnpol)
-reader.ClearData();
+     # normal vector
+    norm = -np.array([l2Data,m2Data,n2Data])
+    norm /= np.sqrt(l2Data**2 + m2Data**2 + n2Data**2)
+    print(norm)
+    total_rays_in_both_axes = xData.shape[0]
 
-# initialize the output (the number of indices in the output array is maxseg*maxseg
-maxseg = 2;
-rays = reader.InitializeOutput(maxseg);
+    # convert to angles of incidence
+    # calculates angle of exitance from direction cosine
+    # the LMN direction cosines are for AFTER refraction
+    # need to calculate via Snell's Law the angle of incidence
+    numerator = (lData*l2Data + mData*m2Data + nData*n2Data)
+    denominator = ((lData**2 + mData**2 + nData**2)**0.5)*(l2Data**2 + m2Data**2 + n2Data**2)**0.5
+    aoe_data = np.arccos(numerator/denominator)
+    aoe = aoe_data - (aoe_data[0:total_rays_in_both_axes] > np.pi/2) * np.pi
+    aoe = np.abs(aoe)
+    aoi = np.arcsin(n2/n1 * np.sin(aoe))
 
-# add the 4 full field marginal rays
-reader.AddRay(wave,
-              Hx,
-              Hy,
-              Px,
-              Py,
-              Enum.Parse(ZOSAPI.Tools.RayTrace.OPDMode,'None'))
+    # Compute kin with Snell's Law: https://en.wikipedia.org/wiki/Snell%27s_law#Vector_form
+    kout = np.array([lData,mData,nData])
+    kout /= np.sqrt(lData**2 + mData**2 + nData**2)
 
-isfinished = False
-while not isfinished:
-    segments = reader.ReadNextBlock(rays)
-    if segments == 0:
-        isfinished = True
+    if mode == 'transmission':
+        kin = np.cos(np.arcsin(n2*np.sin(np.arccos(kout))/n1))
+    elif mode == 'reflection':
+        kin = kout - 2*np.cos(aoi)*norm
 
-print(list(rays.X))
-print(list(rays.Y))
+    return aoi,xData,yData,kin,kout,norm
 
-tool.close()
 
+def ZemaxTraceToSurface(nrays,pth,surf):
+    import numpy as np
+    """Traces to a single surface in Zemax OpticStudio Standalone API.
+
+    Parameters
+    ----------
+    nrays : int
+        how many rays across the pupil that are traced
+    pth : str
+        full file path from C: to your lens file
+    surf : int
+        surface to trace to in ZOS lens file
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    # Some User Imports
+    filename = pth
+    wave = 1
+    x = np.linspace(-1,1,nrays)
+    y = np.linspace(-1,1,nrays)
+    x,y = np.meshgrid(x,y)
+    X = np.ravel(x)
+    Y = np.ravel(y)
+    Px = np.ravel(x)
+    Py = np.ravel(y)
+    # Px = Px[X**2 + Y**2 <= 1]
+    # Py = Py[X**2 + Y**2 <= 1]
+
+    Hx = np.zeros(Px.shape)
+    Hy = np.zeros(Px.shape)
+    # Px,Py = np.meshgrid(Px,Py)
+
+    # add the raytrace.dll from the current directory
+    import clr, os
+    dll = os.path.join(os.path.dirname(os.path.realpath(__file__)), r'RayTrace.dll')
+    # dll = 'RayTrace.dll'
+    # dll = r'C:\Users\Michael\Documents\Temp\KA-01651-Matlab-DLL\12114-Matlab DLL\RayTrace.dll';
+
+    clr.AddReference(dll)
+
+    # import the raytrace namespace
+    import BatchRayTrace
+
+    # load the double gauss
+    zos = zosapi.App()
+    TheSystem = zos.TheSystem
+    ZOSAPI = zos.ZOSAPI
+    TheSystem.LoadFile(filename, False)
+
+    # check to make sure the file was loaded
+    if TheSystem.LDE.NumberOfSurfaces < 4:
+        print('did not load the file properly')
+        exit()
+
+    if surf > TheSystem.LDE.NumberOfSurfaces:
+        print('surf is greater than number of surfaces, setting to last surface')
+        surf = TheSystem.LDE.NumberOfSurfaces
+
+    # run a sequential ray trace
+    maxrays = Px.shape[0];        # let's trace the 4 marginal rays
+    tool = TheSystem.Tools.OpenBatchRayTrace()
+    normUnpol = tool.CreateNormUnpol(maxrays, ZOSAPI.Tools.RayTrace.RaysType.Real, surf)
+
+    # pass raytracer to dll
+    reader = BatchRayTrace.ReadNormUnpolData(tool, normUnpol)
+    reader.ClearData()
+
+    # initialize the output (the number of indices in the output array is maxseg*maxseg
+    maxseg = nrays
+    rays = reader.InitializeOutput(maxseg)
+
+    # add the 4 full field marginal rays
+    import numpy as np
+    reader.AddRay(wave, 
+        Hx, 
+        Hy, 
+        Px, 
+        Py, Enum.Parse(ZOSAPI.Tools.RayTrace.OPDMode, 'None'))
+
+    # read the segments (array length is maxsegs * max segs)
+    isfinished = False
+    while not isfinished:
+        segments = reader.ReadNextBlock(rays)
+        if segments == 0:
+            isfinished = True
+
+    # convert to numpy arrays
+    xData = np.array(list(rays.X))
+    yData = np.array(list(rays.Y))
+    zData = np.array(list(rays.Z))
+
+    lData = np.array(list(rays.L))
+    mData = np.array(list(rays.M))
+    nData = np.array(list(rays.N))
+
+    l2Data = np.array(list(rays.l2))
+    m2Data = np.array(list(rays.m2))
+    n2Data = np.array(list(rays.n2))
+
+    # always close your tools
+    tool.Close()
+
+    # return the rays
+    return xData,yData,zData,lData,mData,nData,l2Data,m2Data,n2Data

test_PRT_mat.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import polutils as pol
+from pythonraytrace import ZOSRayData
 from numpy.linalg import eig,inv
 
 # Test the PRT Computation
@@ -8,10 +9,13 @@ from numpy.linalg import eig,inv
 # n1 = 1.4358 + 1j*9.4953 # Aluminum
 n1 = 0.81257 + 1j*6.0481
 n2 = 1
-pth = 'test_prt_data.txt'
+pth = 'C:/Users/jaren/Desktop/Polarization-Raytrace/Hubble_Test.zmx'
+nrays = 5
+surf = 2
 
 # pth = '/Users/jashcraft/Desktop/prt-data/Webb_Parabola_ray_data.txt'
-aoi,x,y,kin,kout,norm = pol.ConvertBatchRayData(pth,n1,n2,mode='reflection')
+aoi,x,y,kin,kout,norm = pol.ConvertBatchRayData('test_prt_data.txt',n1,n2,mode='reflection')
+# aoi,x,y,kin,kout,norm = ZOSRayData(nrays,pth,surf,n1,n2,mode='reflection')
 ts,tp = pol.FresnelCoefficients(aoi,n1,n2)
 Pmat = np.zeros([3,3,kin.shape[1]],dtype='complex128')
 Jmat = np.zeros([3,3,kin.shape[1]],dtype='complex128')
@@ -27,11 +31,16 @@ for i in range(kin.shape[1]):
         n1,
         n2,
         mode='reflection')
-    # Diagonalize PRT Matrix with eigenvectors
-    eval,evec = eig(P)
-    Pmat[:,:,i] = inv(evec) @ P @ evec
+    # print(P)
+    # # Diagonalize PRT Matrix with eigenvectors
+    # eval,evec = eig(P)
+    Pmat[:,:,i] =  P # inv(evec) @ P @ evec
 
-
+print(aoi)
+plt.figure()
+plt.scatter(x,y,c=aoi)
+plt.colorbar()
+plt.show()
 
 
 fig,axs = plt.subplots(figsize=[9,9],nrows=3,ncols=3)
@@ -57,6 +66,8 @@ for j in range(3):
         ax.axes.yaxis.set_visible(False)
         fig.colorbar(sca,ax=ax)
 plt.show()
+
+
 
 # fig,axs = plt.subplots(figsize=[9,3],ncols=3)
 # plt.suptitle('E field')
@@ -102,35 +113,35 @@ plt.show()
 #     fig.colorbar(sca,ax=ax)
 # plt.show()
 
-fig,axs = plt.subplots(figsize=[9,9],nrows=3,ncols=3)
-plt.suptitle('|Jones Matrix| for Surface in Hubble')
-for j in range(3):
-    for k in range(3):
-        ax = axs[j,k]
-        ax.set_title('J{j}{k}'.format(j=j,k=k))
-        sca = ax.scatter(x,y,c=np.abs(Jmat[j,k,:]))
-        fig.colorbar(sca,ax=ax)
-plt.show()
+# fig,axs = plt.subplots(figsize=[9,9],nrows=3,ncols=3)
+# plt.suptitle('|Jones Matrix| for Surface in Hubble')
+# for j in range(3):
+#     for k in range(3):
+#         ax = axs[j,k]
+#         ax.set_title('J{j}{k}'.format(j=j,k=k))
+#         sca = ax.scatter(x,y,c=np.abs(Jmat[j,k,:]))
+#         fig.colorbar(sca,ax=ax)
+# plt.show()
 
-fig,axs = plt.subplots(figsize=[9,9],nrows=3,ncols=3)
-plt.suptitle('Arg{Jones Matrix} for Surface in Hubble')
-for j in range(3):
-    for k in range(3):
+# fig,axs = plt.subplots(figsize=[9,9],nrows=3,ncols=3)
+# plt.suptitle('Arg{Jones Matrix} for Surface in Hubble')
+# for j in range(3):
+#     for k in range(3):
 
-        # Offset the p coefficient
-        if j == 1:
-            if k == 1:
-                offset = np.pi
-            else:
-                offset = 0
-        else:
-            offset = 0
+#         # Offset the p coefficient
+#         if j == 1:
+#             if k == 1:
+#                 offset = np.pi
+#             else:
+#                 offset = 0
+#         else:
+#             offset = 0
 
-        ax = axs[j,k]
-        ax.set_title('J{j}{k}'.format(j=j,k=k))
-        sca = ax.scatter(x,y,c=np.angle(Jmat[j,k,:])+offset)
-        fig.colorbar(sca,ax=ax)
-plt.show()
+#         ax = axs[j,k]
+#         ax.set_title('J{j}{k}'.format(j=j,k=k))
+#         sca = ax.scatter(x,y,c=np.angle(Jmat[j,k,:])+offset)
+#         fig.colorbar(sca,ax=ax)
+# plt.show()
 
 # Compute Diattenuation and Retardance
 # print(Jmat)